0db157135610acec7f3b63b2943b8801c1856662
Abhinash Yaram <abhinash.yaram@gmail.com>
2026-08-28 15:05:42 +0530

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        # Handle negative exponent
+        if n < 0:
+            x = 1 / x
+            n = -n
+
+        result = 1.0
+
+        # Binary exponentiation
+        while n > 0:
+            # If n is odd
+            if n % 2 == 1:
+                result *= x
+
+            x *= x
+            n //= 2
+
+        return result
